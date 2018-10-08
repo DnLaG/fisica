70cf2171c784b44908a5f8b46cd27e7183b14dd3
owllxz <luxskkz@gmail.com>
2018-10-08 02:21:10 -0300

    Configuracion basica ventana y clases

--- dev/interfaz.py
+++ dev/interfaz.py
@@ -8,8 +8,14 @@ import tkinter as tk
 import ideal as id
 import balistico as bl
 
-class menu():
+class menu(tk.Frame):
     propiedades = main.propiedades() #Importando propiedades del proyecto
     ventana = tk.Tk()
     ventana.title(propiedades.version)
+    ventana.config(bg="white")
+    ventana.config(relief="flat", bd=8) #tipo de borde relief y bd es la medida
+
+    frame = tk.Frame()
+    frame.pack(fill = "both", expand="true")
+    frame.config(bg = "#bfbfbf", width = propiedades.default_size_ancho, height = propiedades.default_size_alto) #Medidas iniciales de la ventana + Color de fondo
     ventana.mainloop()
--- dev/main.py
+++ dev/main.py
@@ -8,6 +8,8 @@ import interfaz as ifz
 
 class propiedades():
     version = "0.0.0_build"
+    default_size_alto = "720"
+    default_size_ancho = "1280"
 
 if __name__ == "__main__":
     menu = ifz.menu() #Llamando a la clase menu para iniciar interfaz
