45bac8ae06ad6d35db3eba9b832ddb13be61ce20
owllxz <luxskkz@gmail.com>
2018-10-08 01:50:30 -0300

    Update

--- dev/interfaz.py
+++ dev/interfaz.py
@@ -2,8 +2,14 @@
     Version Sofware: 0.0.0
     Version Python: 3.7
 """
-#!/usr/bin/env python3
 
+import main
 import tkinter as tk
 import ideal as id
 import balistico as bl
+
+class menu():
+    propiedades = main.propiedades() #Importando propiedades del proyecto
+    ventana = tk.Tk()
+    ventana.title(propiedades.version)
+    ventana.mainloop()
--- dev/main.py
+++ dev/main.py
@@ -5,3 +5,9 @@
 #!/usr/bin/env python3
 
 import interfaz as ifz
+
+class propiedades():
+    version = "0.0.0_build"
+
+if __name__ == "__main__":
+    menu = ifz.menu() #Llamando a la clase menu para iniciar interfaz
