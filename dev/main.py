73dded5ce06109e4f9533cd9e4a08d69abab8f98
owllxz <luxskkz@gmail.com>
2018-10-08 00:59:38 -0300

    Cambiando ubicacion

--- dev/main.py
+++ dev/main.py
@@ -1,1 +1,7 @@
+"""
+    Version Sofware: 0.0.0
+    Version Python: 3.7
+"""
+#!/usr/bin/env python3
 
+import interfaz as ifz
--- main.py
+++
@@ -1,7 +0,0 @@
-"""
-    Version Sofware: 0.0.0
-    Version Python: 3.7
-"""
-#!/usr/bin/env python3
-
-import interfaz as ifz
