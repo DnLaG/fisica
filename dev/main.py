9fd5d2366b5e0537242f3b51b5da4c153d28ab8e
Omar Olivares <ofjolivares@gmail.com>
2018-10-10 23:33:50 -0300

    Corrección del archivo main

--- dev/main.py
+++ dev/main.py
@@ -1,15 +1,11 @@
-"""
-    Version Sofware: 0.0.0
-    Version Python: 3.7
-"""
 #!/usr/bin/env python3
 
 from interfaz import *
 
 class propiedades():
     version = "0.0.0_build"
-    default_size_alto = "720"
-    default_size_ancho = "1280"
+    default_size_alto = "600"
+    default_size_ancho = "800"
 
 if __name__ == "__main__":
     pass
