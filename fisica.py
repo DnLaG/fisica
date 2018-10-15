957b73a7a7cfdb929152e8d41a70b27bab1e516d
Omar <ofjolivares@gmail.com>
2018-10-15 19:41:55 -0300

    Motor de visualización inspiración

--- fisica.py
+++ fisica.py
@@ -1,12 +1,10 @@
 from interface import admin
 from interface import ideal
 from interface import balistica
-from graphics import motor
 
 if __name__ == '__main__':
     # Create the entire GUI program
     program = ideal.Interface()
-    viz = motor.window
 
     # Start the GUI event loop
     program.window.mainloop()
