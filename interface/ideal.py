7ac8ae6a47cd58cc73324b9cd0b06abb122cb296
Omar Olivares <ofjolivares@gmail.com>
2018-10-21 14:15:30 -0300

    Añadir botones de funciones a implementar

--- interface/ideal.py
+++ interface/ideal.py
@@ -15,12 +15,19 @@ class Interface:
         self.opciones = ttk.Frame(self.tab_ideal)
 
         # Inicializar los botones de la interfaz
+        self.boton_posicion = ttk.Button(self.opciones, text="Posición", width=10)
         self.boton_velocidad = ttk.Button(self.opciones, text="Velocidad", width=10)
+        self.boton_aceleracion = ttk.Button(self.opciones, text="Aceleración", width=10)
+        self.boton_alcance_horizontal = ttk.Button(self.opciones, text="Alcance Horizontal", width=10)
+        self.boton_altura_maxima = ttk.Button(self.opciones, text="Altura Màxima", width=10)
+        self.boton_camino_recorrido = ttk.Button(self.opciones, text="Camino Recorrido", width=10)
+        self.boton_radio_y_centro_de_curvatura = ttk.Button(self.opciones, text="Radio y Centro de Curvatura", width=10)
+        self.boton_aceleracion_normal_y_tangencial = ttk.Button(self.opciones, text="A. normal y tangencial", width=10)
+        self.boton_vector_normal = ttk.Button(self.opciones, text="Vector normal", width=10)
 
         self.create_widgets()
 
     def create_widgets(self):
-
         self.pestañas.pack(side=tk.TOP, fill=tk.BOTH, expand=True, ipadx=10, ipady=10)
 
         tab_balistica = tk.Frame(self.pestañas)
@@ -35,7 +42,15 @@ class Interface:
 
         self.opciones.pack(side=tk.RIGHT, fill=tk.BOTH, expand=False, padx=5, pady=5)
 
+        self.boton_posicion.pack(side=tk.TOP, padx=10, pady=10)
         self.boton_velocidad.pack(side=tk.TOP, padx=10, pady=10)
+        self.boton_aceleracion.pack(side=tk.TOP, padx=10, pady=10)
+        self.boton_alcance_horizontal.pack(side=tk.TOP, padx=10, pady=10)
+        self.boton_altura_maxima.pack(side=tk.TOP, padx=10, pady=10)
+        self.boton_camino_recorrido.pack(side=tk.TOP, padx=10, pady=10)
+        self.boton_radio_y_centro_de_curvatura.pack(side=tk.TOP, padx=10, pady=10)
+        self.boton_aceleracion_normal_y_tangencial.pack(side=tk.TOP, padx=10, pady=10)
+        self.boton_vector_normal.pack(side=tk.TOP, padx=10, pady=10)
 
         graphics = ttk.LabelFrame(self.tab_ideal, text="Gráfica")
         graphics.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=5, pady=5)
@@ -76,7 +91,7 @@ class Interface:
         # var_y.bind("<ButtonRelease-1>", update_angle_value)
 
         aceleracion_entry = ttk.Entry(aceleracion, justify=tk.CENTER)
-        aceleracion_entry.pack(side=tk.TOP,fill=tk.BOTH, expand=True)
+        aceleracion_entry.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         aceleracion_entry.insert(tk.END, "Aceleración")
         var_y = ttk.Scale(aceleracion, from_=0, to=100, orient=tk.HORIZONTAL)
         var_y.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=10, pady=10)
